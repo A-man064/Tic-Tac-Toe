d0ca5353aa87e38384762fdfd158ebd7e15a731f
A-man064 <adam.g.stramel@gmail.com>
2025-08-27 16:29:56 -0700

    Updated Main Menu to collect an Input from the user

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,12 +1,19 @@
 import check_input
 import random
 
-
+'''Displays the main menu for the player to pick a game mode of view scores'''
 def menu():
     print("TIC-TAC-TOE\n")
     print("1. Player vs Player")
     print("2. Player vs Computer")
     print("3. View Scores")
+    user_input = check_input.check_int_range("Select an option (1-3): ", 1, 3)
+    if user_input == 1:
+        return 1
+    elif user_input == 2:
+        return 2
+    elif user_input == 3:
+        return 3
 
 
 
