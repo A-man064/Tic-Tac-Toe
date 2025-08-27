a70aa6bc3c098aaf8a237490fc6c4920364e539e
A-man064 <adam.g.stramel@gmail.com>
2025-08-27 16:40:24 -0700

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,5 +1,6 @@
 import check_input
 import random
+import sys
 
 '''Displays the main menu for the player to pick a game mode of view scores'''
 def menu():
@@ -7,7 +8,7 @@ def menu():
     print("1. Player vs Player")
     print("2. Player vs Computer")
     print("3. View Scores")
-    user_input = check_input.check_int_range("Select an option (1-3): ", 1, 3)
+    user_input = check_input.get_int_range("Select an option (1-3): ", 1, 3)
     if user_input == 1:
         return 1
     elif user_input == 2:
