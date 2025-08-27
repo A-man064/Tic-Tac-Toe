ff4b5499a0afbfaf89bfa8e9c3af8f438421025b
A-man064 <adam.g.stramel@gmail.com>
2025-08-27 16:22:40 -0700

    Completed Main menu

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,21 @@
+import check_input
+import random
+
+
+def menu():
+    print("TIC-TAC-TOE\n")
+    print("1. Player vs Player")
+    print("2. Player vs Computer")
+    print("3. View Scores")
+
+
+
+
+def main():
+    menu()
+    
+
+main()
+    
+
+    
