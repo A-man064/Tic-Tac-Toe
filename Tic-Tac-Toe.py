e7b7f7e7a46100a4dacccccdeb420b27911e2409
A-man064 <adam.g.stramel@gmail.com>
2025-08-27 16:53:26 -0700

    Start of development for board features
    The Current plan is to fill up slots with user inputs and computer inputs and place them on the board
    X: slots = [slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9]

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -8,6 +8,8 @@ def menu():
     print("1. Player vs Player")
     print("2. Player vs Computer")
     print("3. View Scores")
+    
+    #This function checks if the user input is valid and returns the corresponding option
     user_input = check_input.get_int_range("Select an option (1-3): ", 1, 3)
     if user_input == 1:
         return 1
@@ -15,7 +17,7 @@ def menu():
         return 2
     elif user_input == 3:
         return 3
-
+    #My style of coding is to always return inputs to main function and have that act as an intermediary between functions
 
 
 
@@ -23,7 +25,18 @@ def main():
     menu()
     
 
-main()
-    
 
     
+'''Prints a board with slots to be filled up by user or computer inputs'''
+def print_board(slots):
+    print("\n")
+    print(" " + slots[0] + " | " + slots[1] + " | " + slots[2] + " ")
+    print("---+---+---")
+    print(" " + slots[3] + " | " + slots[4] + " | " + slots[5] + " ")
+    print("---+---+---")
+    print(" " + slots[6] + " | " + slots[7] + " | " + slots[8] + " ")
+    print("\n")    
+
+print_board([])
+
+]
